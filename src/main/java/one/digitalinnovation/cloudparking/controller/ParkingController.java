3c78c612b392ccb3d8b1195d91da9caac735ee76
Adriana Guedes <88890733+Adriana-Guedes@users.noreply.github.com>
2022-10-30 12:04:44 -0300

    Update ParkingController.java

--- src/main/java/one/digitalinnovation/cloudparking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/cloudparking/controller/ParkingController.java
@@ -102,12 +102,12 @@ public class ParkingController {
 		}
 	
 	//METODO DE SAIDA DO ESTACIONAMENTO
-	@PostMapping("/{id}")
-	public  ResponseEntity<ParkingDTO> exit(@PathVariable String id) {
-			Parking parking = parkingService.exit(id);
-			return ResponseEntity.ok(parkingMapper.toParkingDTO(parking));
-		
-		}
+ 	@PostMapping("/{id}/exit")
+    	public ResponseEntity<ParkingDTO> checkOut(@PathVariable String id) {
+        //TODO verificar se já não esta fechado e lançar exceção
+        Parking parking = parkingService.checkOut(id);
+        return ResponseEntity.ok(parkingMapper.toParkingDTO(parking));
+    }
 	
 	
 		
